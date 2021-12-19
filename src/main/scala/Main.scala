9ccbf00e3c620e182db4bde351070582bfc707e0
naylepsh <naylepsh@gmail.com>
2021-12-19 12:17:01 +0100

    add screen capturer

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+import ScreenCapture._
+
+import java.awt.Point
+
+object Main extends App {
+  val artifactCoordinates = RectangleCoordinates(new Point(1295, 120), new Point(1780, 670))
+  val image = ScreenCapture.captureRectangle(artifactCoordinates)
+  saveToFile(image, "./artifact")
+  println("Done")
+}
