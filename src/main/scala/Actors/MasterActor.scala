9ac4d17bf14cb48615414029682801872939703c
naylepsh <naylepsh@gmail.com>
2022-01-15 20:03:46 +0100

    attach extractor to master

--- src/main/scala/Actors/MasterActor.scala
+++ src/main/scala/Actors/MasterActor.scala
@@ -14,6 +14,7 @@ import scala.util.Try
 class MasterActor(scanner: ArtifactScannable, extractor: ArtifactFromImageExtractable with NumberExtractable)
   extends Actor with ActorLogging {
 
+  import ArtifactExtractorActor._
   import ArtifactScannerActor._
   import MasterActor._
 
@@ -22,16 +23,46 @@ class MasterActor(scanner: ArtifactScannable, extractor: ArtifactFromImageExtrac
   private val itemsNumberCoordinates = RectangleCoordinates(new Point(1685, 35), new Point(1740, 60))
 
   override def receive: Receive = {
-    case Start =>
-      val numberTry = extractNumberOfItemsToScan()
-      if (numberTry.isFailure)
-        log.error("Failed to extract the number of artifacts")
-      else {
-        val artifactsToScan = numberTry.get
-        context.become(receiveWithResults(List(), artifactsToScan))
-        scannerActor ! StartScanning(artifactsToScan)
-      }
-    case ArtifactScanned(filename) => self ! ArtifactScanned(filename)
+    /**
+     * It's possible that the master actor got the message from scanner before it managed to change its context.
+     * In such cases simply putting the message back on the queue hoping that the context changes before it arrives
+     * should do the trick.
+     *
+     * Something like `case other: ArtifactScanned => self ! other` would be cleaner, but the compiler complains.
+     */
+    case Start => start()
+    case ArtifactScanned(filename) => putBackOnQueue(ArtifactScanned(filename))
+    case other => putBackOnQueue(other)
+  }
+
+  def receiveWithResults(artifacts: List[Artifact], artifactsExpected: Int): Receive = {
+    case ArtifactScanned(filename) =>
+      extractorActor ! ExtractArtifact(filename)
+    case ArtifactExtractionSuccess(artifact) =>
+      log.info(s"${artifactsExpected - 1} artifacts left to extract.")
+      context.become(receiveWithResults(artifact :: artifacts, artifactsExpected - 1))
+    case ArtifactExtractionFailure(failure) =>
+      log.error(s"Failed artifact extraction due to $failure")
+      log.info(s"${artifactsExpected - 1} artifacts left to extract.")
+      context.become(receiveWithResults(artifacts, artifactsExpected - 1))
+    case ScanningComplete =>
+      log.info("Done")
+  }
+
+  private def start(): Unit = {
+    val numberTry = extractNumberOfItemsToScan()
+    if (numberTry.isFailure)
+      log.error("Failed to extract the number of artifacts")
+    else {
+      val artifactsToScan = numberTry.get
+      log.info(s"Attempting to scan $artifactsToScan artifacts")
+      context.become(receiveWithResults(List(), artifactsToScan))
+      scannerActor ! StartScanning(artifactsToScan)
+    }
+  }
+
+  private def putBackOnQueue(message: Any): Unit = {
+    self ! message
   }
 
   private def extractNumberOfItemsToScan(): Try[Int] = {
@@ -40,11 +71,6 @@ class MasterActor(scanner: ArtifactScannable, extractor: ArtifactFromImageExtrac
 
   private def scanItemNumber(): BufferedImage = {
     ScreenCapture.captureRectangle(itemsNumberCoordinates)
-  }
-
-  def receiveWithResults(artifacts: List[Artifact], artifactsExpected: Int): Receive = {
-    case ArtifactScanned(filename) => log.info(s"Acquired $filename")
-    case ScanningComplete => log.info("Done")
   }
 }
 
