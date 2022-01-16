d42c630e61540988b59e142fee3c85040701ef6c
naylepsh <naylepsh@gmail.com>
2022-01-16 10:27:51 +0100

    add image hashing for future image comparison

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,8 +3,11 @@ import Actors.MasterActor.Start
 import Extraction._
 import Scan.ArtifactScanner
 import akka.actor.ActorSystem
+import org.apache.commons.io.output.ByteArrayOutputStream
 
+import java.awt.image.BufferedImage
 import java.io.File
+import java.security.MessageDigest
 import javax.imageio.ImageIO
 
 object Main extends App {
@@ -14,7 +17,6 @@ object Main extends App {
   val extractor = ArtifactOCRExtractor(tesseract)
   val outputDir = sys.env("OUTPUT_DIR")
   val scanner = ArtifactScanner(outputDir)
-  val system = ActorSystem("GenshinArtifactScanner")
 
   def actorless(): Unit = {
     val itemsNumberFilename = s"$outputDir/n.png"
@@ -48,5 +50,47 @@ object Main extends App {
     master ! Start
   }
 
-  withActor()
+  //  withActor()
+
+  def imageHash(): Unit = {
+    val path = "F:/Code/genshin-artifact-scanner/src/test/resources/artifacts/5-star-4-stats-goblet.png"
+    val image = ImageIO.read(new File(path))
+    println(computeImageHash(image))
+    val otherImage = ImageIO.read(new File(path))
+    println(computeImageHash(otherImage))
+    val differentPath = "F:/Code/genshin-artifact-scanner/src/test/resources/artifacts/5-star-4-stats-flower.png"
+    val differentImage = ImageIO.read(new File(differentPath))
+    println(computeImageHash(differentImage))
+  }
+
+  def computeImageHash(image: BufferedImage): String = {
+    val data = imageToByteArray(image)
+    val hash = hashBytes(data)
+    bytesToHexString(hash)
+  }
+
+  def hashBytes(bytes: Array[Byte]): Array[Byte] = {
+    val md = MessageDigest.getInstance("MD5")
+    md.update(bytes)
+    val hash = md.digest
+    hash
+  }
+
+  def imageToByteArray(image: BufferedImage): Array[Byte] = {
+    val outputStream = new ByteArrayOutputStream()
+    ImageIO.write(image, "png", outputStream)
+    outputStream.toByteArray
+  }
+
+  def bytesToHexString(bytes: Array[Byte]): String = {
+    def byteToHexString(byte: Byte): String = {
+      Integer.toString((byte & 0xff) + 0x100, 16).substring(1)
+    }
+
+    bytes.map(byteToHexString).mkString("")
+  }
+
+
+  imageHash()
 }
+
